cef995009e8c9cce02d39e78ce28b481b0f6e748
Joshua Mack <mackncheesiest@gmail.com>
2026-04-30 18:55:02 -0700

    Adjusted base path for CNAME

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tucsonmesh-configgen/",
+  base: "/",
   plugins: [react()],
   build: {
     outDir: "build",
